0be626f8873418690f677dc0025fb063abdaf447
Jitendrasinh Rajparmar <125237946+Jitendra7666@users.noreply.github.com>
2024-11-22 14:26:13 +0530

    First Commit

--- src/BrowserOpen.java
+++ src/BrowserOpen.java
@@ -1,12 +1,16 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class BrowserOpen
 {
     public static void main(String[] args)
     {
-        WebDriver driver = new ChromeDriver();
+        //WebDriver driver = new ChromeDriver();
+        WebDriver driver = new FirefoxDriver();
         driver.manage().window().maximize();
+
+
 
         driver.get("http://www.facebook.com");
     }
